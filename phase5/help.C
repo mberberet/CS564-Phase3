abbaeda11c94723ac8b1e236dce2b986db814ef6
clanham <the.other.underdog@gmail.com>
2014-04-13 13:34:57 -0500

    working on hlep

--- phase5/help.C
+++ phase5/help.C
@@ -35,7 +35,7 @@ const Status RelCatalog::help(const string & relation)
     if (relation.empty()) {
         return UT_Print(RELCATNAME);
     } else{
-        status = getRelInfo(relation, attrCnt, attrs);
+        status = attrCat -> getRelInfo(relation, attrCnt, attrs);
         if (status != OK){
             return status;
         }
@@ -47,7 +47,7 @@ const Status RelCatalog::help(const string & relation)
         printf("attrName    attrOffset   attrType    attrLen \n");
         printf("----------- ---------- ---------- ----------\n");
         for (int i = 0;i < attrCnt; i++){
-            printf("%s %*d %*d %*d", attrs[i].attrName, 21, attrs[i].attrOffset, 32, attrs[i].attrType, 43, attrs[i].attrLen);
+            printf("%s %-*d %-*d %-*d\n ", attrs[i].attrName, 20, attrs[i].attrOffset, 22 , attrs[i].attrType, 43, attrs[i].attrLen);
         }
     }   
     return OK;
