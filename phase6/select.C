9691968c6974ae77b6611219ac27d605479a606a
clanham <the.other.underdog@gmail.com>
2014-04-23 16:57:56 -0500

    select

--- phase6/select.C
+++ phase6/select.C
@@ -1,6 +1,7 @@
 #include "catalog.h"
 #include "query.h"
-
+#include "stdio.h"
+#include "stdlib.h"
 
 // forward declaration
 const Status ScanSelect(const string & result, 
@@ -29,15 +30,17 @@ const Status QU_Select(const string & result,
    // Qu_Select sets up things and then calls ScanSelect to do the actual work
     Status status;
     AttrDesc attrDescArray[projCnt];
-    AttrDesc *attrDesc;
-    int attrCnt;
+    AttrDesc attrDesc;
     int reclen = 0;
     const char* filter;
+    
     cout << "Doing QU_Select " << endl;   
     for (int i = 0; i < projCnt; i++)
     {
-        status = attrCat -> getInfo(projNames[i].relName,
-                                    proNames[i].attrName,
+        string relation(projNames[i].relName);
+        string attrName(projNames[i].attrName);
+        status = attrCat -> getInfo(relation,
+                                    attrName,
                                     attrDescArray[i]);
         if (status != OK)
         {   
@@ -46,31 +49,32 @@ const Status QU_Select(const string & result,
         reclen += attrDescArray[i].attrLen; 
 
     }
-    status = attrCat -> getInfo(attr->relName, attr->attrName, attrDesc); 
+    string relation(attr->relName);
+    string attrName(attr->attrName);
+    status = attrCat -> getInfo(relation,attrName, attrDesc); 
     if (status != OK)
         {   
             return status;
         }
 
-    if (type == INTEGER) {
+    if (attr->attrType == INTEGER) {
         int val = atoi(attrValue);
         filter = (char *)&val;
-    } else if (type == FLOAT) {
+    } else if (attr->attrType == FLOAT) {
         double val = atof(attrValue);
         filter = (char *)&val;
     } else {
         filter = attrValue;
     }
     status = ScanSelect(result, projCnt, attrDescArray, 
-                        attrDesc, op, filter, reclen);          
+                       & attrDesc, op, filter, reclen);          
     return status;        
 }
  
 
 
 const Status ScanSelect(const string & result, 
-#include "stdio.h"
-#include "stdlib.h"
+
 			const int projCnt, 
 			const AttrDesc projNames[],
 			const AttrDesc *attrDesc, 
@@ -84,14 +88,14 @@ const Status ScanSelect(const string & result,
     Record rec;
     RID rid;
     InsertFileScan* ifs;
+    Record insRec;
+
 
     ifs = new InsertFileScan(result, status);
     if (status != OK) {
         return status;
     }
-
-
-    hfs = new HeapFileScan(projNames[1].relName, status);
+    hfs = new HeapFileScan(projNames[0].relName, status);
     if (status != OK) {
         return status;
     }
@@ -101,11 +105,23 @@ const Status ScanSelect(const string & result,
     if (status != OK) {
         return status;
     }
+    insRec.data = (char*) malloc(reclen);
+    insRec.length = reclen;
     while ((status = hfs->scanNext(rid)) == OK) {
         status = hfs->getRecord(rec);
-    if (status != OK) {
-        return status;
+        int offset = 0;
+        if (status != OK) {
+            return status;
+        }
+        for (int i = 0; i < projCnt; i++){
+            memcpy((char*)insRec.data + offset,(char*) rec.data + projNames[i].attrOffset, 
+                    projNames[i].attrLen );
+            offset = offset + projNames[i].attrLen;
+            status = ifs->insertRecord(insRec, rid);
+            if (status != OK) {
+                return status;
+            }
+        }   
     }
-    
-
+    return status;
 }
